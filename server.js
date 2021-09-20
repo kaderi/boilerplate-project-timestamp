50f43d21b542eaa71e7947320c62ca295f0e25ef
El mostafa El kaderi Yemlahi <elmostafaelkaderi@Gmail.com>
2021-09-20 22:11:22 +0000

    created code to pass the challenge

--- server.js
+++ server.js
@@ -22,11 +22,47 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+  console.log({greeting: 'hello API'});
 });
 
+//empty request
+app.get("/api", (req, res) => {
+  var currentDate = new Date();
+  res.json({
+    "unix": currentDate.getTime(),
+    "utc": currentDate.toUTCString()
+  });
 
+});
+
+app.get("/api/:date", (req, res) => {
+
+  let date = req.params.date;
+  // to handle get with an integer 
+  if(parseInt(date) > 10000){
+    let unix = new Date(parseInt(date));
+    res.json({
+      "unix": unix.getTime(),
+      "utc": unix.toUTCString()
+
+    });
+  }
+  let passedValue = new Date(date);
+  console.log(date);
+
+  if (passedValue == "Invalid Date"){
+     res.json({"error" : "Invalid Date" });
+  }else{
+    res.json({
+      "unix": passedValue.getTime(),
+      "utc": passedValue.toUTCString()
+    })
+  }
+  
+  
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
